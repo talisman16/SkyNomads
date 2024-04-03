a92fed208ae0398ca14c185e6e38058f657c4e13
talisman16 <nassimdev16@gmail.com>
2024-04-03 16:34:08 +0000

    Init de projet Spring boot

---
+++ src/main/java/com/example/NomadicSKy/NomadicSKyApplication.java
@@ -0,0 +1,13 @@
+package com.example.NomadicSKy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NomadicSKyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NomadicSKyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/NomadicSKy/NomadicSKyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.NomadicSKy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NomadicSKyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
