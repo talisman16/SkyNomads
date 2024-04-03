d337c7788ee0615cdf76380901affdb75b193a92
talisman16 <nassimdev16@gmail.com>
2024-04-03 23:21:35 +0000

    AiroportModel

--- src/main/java/com/example/NomadicSKy/Models/Aéroport.java
+++ src/main/java/com/example/NomadicSKy/Models/Aéroport.java
@@ -1,6 +1,72 @@
 package com.example.NomadicSKy.Models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
 
+@Entity 
+@Table(name = "Aeroport" , uniqueConstraints = {@UniqueConstraint(columnNames = "nom_Aeroport" )})
 public class Aéroport {
-    
+  @Id
+  @GeneratedValue(strategy=GenerationType.AUTO)
+  private Integer Aeroport_id;
+
+  @Column( name = "nom_Aeroport" , unique = true , nullable= false) 
+  private String nom_Aeroport;
+ 
+  private String Aeroport_ville;
+
+  private String Aeroport_pays;
+
+
+  // Getters 
+
+  // id de l'aeroport 
+  public Integer getIdAeroport()
+  {
+    return this.Aeroport_id;
+  }
+//  ville de l'aeroport 
+  public String getAeroportVille()
+  {
+    return this.Aeroport_ville;
+  }
+// pays de l'areoport 
+
+public String getPaysAeroport()
+{
+  return this.Aeroport_pays;
+}
+  // nom de l'aeroport
+    public String getNomAeroport()
+    {
+      return this.nom_Aeroport; 
+    }
+
+
+// Setters 
+
+public void setIdAeroport(Integer id) {
+  this.Aeroport_id = id;
+}
+
+// Setter for nom_Aeroport
+public void setNomAeroport(String nomAeroport) {
+  this.nom_Aeroport = nomAeroport;
+}
+
+// Setter for Aeroport_ville
+public void setAeroportVille(String ville) {
+  this.Aeroport_ville = ville;
+}
+
+// Setter for Aeroport_pays
+public void setPaysAeroport(String pays) {
+  this.Aeroport_pays = pays;
+}
+
 }
