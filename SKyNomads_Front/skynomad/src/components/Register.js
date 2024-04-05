12e7fdbce27154704c928bf96ebde6632cbfbeeb
talisman16 <nassimdev16@gmail.com>
2024-04-05 22:36:21 +0000

    Country_Api_Register

--- SKyNomads_Front/skynomad/src/components/Register.js
+++ SKyNomads_Front/skynomad/src/components/Register.js
@@ -1,12 +1,60 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import axios from 'axios';
+
 const Register = () => {
+  const [countries, setCountries] = useState([]);
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [nom, setNom] = useState('');
+  const [prenom, setPrenom] = useState('');
+  const [selectedCountry, setSelectedCountry] = useState('');
+  const [selectedCity, setSelectedCity] = useState('');
+
+
+  useEffect(() => {
+    fetch('https://restcountries.com/v3.1/all?fields=name')
+      .then((response) => response.json())
+      .then((data) => setCountries(data));
+  }, []);
+
+  const handleCountryChange = (event) => {
+    setSelectedCountry(event.target.value);
+  };
+
+  const handleRegister = (event) => {
+    event.preventDefault();
+    if (!email || !password || !nom || !prenom || !selectedCountry) {
+      toast.error('Please fill in all fields');
+      return;
+    }
+
+    if (!/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/.test(email)) {
+      toast.error('Please enter a valid email address');
+      return;
+    }
+
+    if (password.length < 8) {
+      toast.error('Please enter a password with at least 8 characters');
+      return;
+    }
+
+    if (nom.length < 5 || prenom.length < 5) {
+      toast.error('Nom or prenom must be at least 5 characters long');
+      return;
+    }
+
+    // Rest of the function
+  };
+
   return (
     <div className="bg-white shadow-md rounded-lg p-6 max-w-md w-full mx-auto">
       <h2 className="text-3xl font-bold text-center mb-6 text-green-600">
         Register
       </h2>
-      <form>
+      <form onSubmit={handleRegister}>
         <div className="grid grid-cols-2 gap-4">
           <div className="mb-4">
             <label htmlFor="nom" className="block text-green-600 font-bold mb-1">
@@ -16,8 +64,10 @@ const Register = () => {
               type="text"
               id="nom"
               name="nom"
+              value={nom}
               className="appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               placeholder="Enter your name"
+              onChange={(event) => setNom(event.target.value)}
             />
           </div>
           <div className="mb-4">
@@ -28,8 +78,10 @@ const Register = () => {
               type="text"
               id="prenom"
               name="prenom"
+              value={prenom}
               className="appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               placeholder="Enter your last name"
+              onChange={(event) => setPrenom(event.target.value)}
             />
           </div>
           <div className="mb-4 col-span-2">
@@ -45,27 +97,35 @@ const Register = () => {
             />
           </div>
           <div className="mb-4">
-            <label htmlFor="ville" className="block text-green-600 font-bold mb-1">
+  <label htmlFor="pays" className="block text-green-600 font-bold mb-1">
+    Pays
+  </label>
+  <select
+    id="pays"
+    name="pays"
+    className="block w-full py-2 px-3 text-gray-700 leading-tight border-gray-300 bg-white rounded-lg focus:outline-none focus:shadow-outline"
+    onChange={handleCountryChange}
+  >
+    <option value="">Select a country</option>
+    {countries.map((country) => (
+      <option key={country.name.common} value={country.name.common}>
+        {country.name.common}
+      </option>
+    ))}
+  </select>
+</div>
+          <div className="mb-4">
+            <label htmlFor="city" className="block text-green-600 font-bold mb-1">
               Ville
             </label>
             <input
               type="text"
-              id="ville"
-              name="ville"
+              id="city"
+              name="city"
+              value={selectedCity}
               className="appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               placeholder="Enter your city"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="pays" className="block text-green-600 font-bold mb-1">
-              Pays
-            </label>
-            <input
-              type="text"
-              id="pays"
-              name="pays"
-              className="appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Enter your country"
+              onChange={(event) => setSelectedCity(event.target.value)}
             />
           </div>
           <div className="mb-4 col-span-2">
@@ -76,20 +136,10 @@ const Register = () => {
               type="email"
               id="email"
               name="email"
+              value={email}
               className="appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
               placeholder="Enter your email"
-            />
-          </div>
-          <div className="mb-4">
-            <label htmlFor="phone" className="block text-green-600 font-bold mb-1">
-              Téléphone
-            </label>
-            <input
-              type="tel"
-              id="phone"
-              name="phone"
-              className="appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
-              placeholder="Enter your phone number"
+              onChange={(event) => setEmail(event.target.value)}
             />
           </div>
           <div className="mb-6 col-span-2">
@@ -100,8 +150,10 @@ const Register = () => {
               type="password"
               id="password"
               name="password"
+              value={password}
               className="appearance-none border rounded-lg w-full py-2 px-3 text-gray-700 mb-1 leading-tight focus:outline-none focus:shadow-outline"
               placeholder="Enter your password"
+              onChange={(event) => setPassword(event.target.value)}
             />
             <p className="text-gray-500 text-xs italic">
               Password must be at least 8 characters long and contain a number, a special character, and a letter.
@@ -122,8 +174,9 @@ const Register = () => {
           Already have an account? <Link to="/login" className="text-green-600 hover:underline">Login here</Link>
         </p>
       </div>
+      <ToastContainer />
     </div>
   );
-}
+};
 
 export default Register;
