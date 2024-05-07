0157c2d1ad7ad85718934453151d8ca72cf2ff72
talisman16 <nassimdev16@gmail.com>
2024-05-07 19:40:53 +0000

    frontv2

--- frontv2/src/App.js
+++ frontv2/src/App.js
@@ -1,29 +1,14 @@
- 
-import   Navbar from "./components/navbar";
-import Login from "./pages/login";
-import Register from "./components/register";
-import Footer from "./components/footer";
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import HeroSection from "./pages/landing";
- 
+  import NavBar from "./components/Navbar";
+  import { ChakraProvider } from '@chakra-ui/react'
 function App() {
   return (
-    
-    <Router>
-          <Navbar/>
-
-      <Routes >
-   
-      <Route exact path="/" element={<HeroSection/>} />
-      <Route exact path="/register" element={< Register/>} />
-    <  Route exact path="/login" element={< Login/>} />
-    
-      </Routes>
-    <Footer/>
-    </Router>
-    
+    <ChakraProvider>
+    <div className="App">
+      <NavBar/>
+        <h1> Hello Nassim </h1>
+    </div>
+    </ChakraProvider>
   );
 }
 
 export default App;
- 
