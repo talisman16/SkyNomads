cb96f5c7913cb8fa7dace2aa898b3de53abdc134
talisman16 <nassimdev16@gmail.com>
2024-05-13 21:47:42 +0000

    DeletzeDFIle

--- SKyNomads_Front/skynomad/src/components/LandingPage.js
+++ SKyNomads_Front/skynomad/src/components/LandingPage.js
@@ -14,7 +14,7 @@ const LandingPage = () => {
 
   return (
     <div className="bg-white dark:bg-gray-800 min-h-screen">
-      <section className="bg-cover bg-center h-screen flex items-center justify-center" style={{ backgroundImage: `url(${process.env.PUBLIC_URL + 'Logo/bg.png'})` }}>
+      <section className="bg-cover bg-center h-screen flex items-center justify-center" style={{ backgroundImage: `url(${process.env.PUBLIC_URL + 'Logo/airlinebgems.jpg'})` }}>
         <div className="text-center bg-gray-900/75 p-8 rounded-lg shadow-2xl">
           <h1 className="text-6xl font-bold text-white">Welcome to SkyNomads</h1>
           <p className="text-gray-300 mt-4">Find and book the best flights for your next adventure</p>
